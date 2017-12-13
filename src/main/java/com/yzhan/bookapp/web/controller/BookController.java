abec1b51f578242ac5797df340e2ad5e8f6ec012
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-12-13 18:38:37 -0500

    added gitattributes

--- src/main/java/com/yzhan/bookapp/web/controller/BookController.java
+++ src/main/java/com/yzhan/bookapp/web/controller/BookController.java
@@ -13,6 +13,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -86,6 +87,7 @@ public class BookController {
         return "redirect:/books/";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @RequestMapping(params = "form", method = RequestMethod.GET)
     public String createForm(Model uiModel) {
         Book book = new Book();
